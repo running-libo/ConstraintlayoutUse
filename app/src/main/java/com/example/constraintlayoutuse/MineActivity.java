c76ff09c37bc5a383493fe57e24b3c52e4f0c46e
apple <18201497620@163.com>
2020-08-02 00:31:23 +0800

    initCommit

---
+++ app/src/main/java/com/example/constraintlayoutuse/MineActivity.java
@@ -0,0 +1,14 @@
+package com.example.constraintlayoutuse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
